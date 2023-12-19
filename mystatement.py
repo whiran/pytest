ded267b9fab3d9c9ebc88933ad26a9b2ffe753c2
Indika Hiran Wijesinghe <hiranart@gmail.com>
2023-12-19 19:27:25 +0530

    Commit message

---
+++ mystatement.py
@@ -0,0 +1,9 @@
+my_list=["apple","orange","banana"]
+my_dict={"drink":"apple Juice","orange":"orange Juice","banana":"Banan smoothee"}
+
+
+for i in my_list:
+    print("fruit:",i)
+
+for k,d in my_dict.items():
+    print(f"{k}:",d)
